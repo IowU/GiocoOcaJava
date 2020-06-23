53a5b8cf83594c268102783ab368fb13e7b96561
Andrea Mucciarelli <andrea.mucciar@gmail.com>
2020-06-23 09:34:21 +0200

    Added movement effect control
    Proposal for the movement loop where a player could be stuck moving between two slots that have the same + and - value

--- src/MovePlayerSlot.java
+++ src/MovePlayerSlot.java
@@ -1,10 +1,22 @@
 import java.util.Random;
 
-public class MovePlayerSlot extends Slot{
-public MovePlayerSlot(){
-    Random random=new Random();
-    int randomNumber=(random.nextInt(Board.MAX_SLOT -1));
-    int randomNumberFinal =(randomNumber - (randomNumber * 2 * random.nextInt(1)));
-    setEffetto(randomNumberFinal);
-}
+public class MovePlayerSlot extends Slot {
+    public MovePlayerSlot() {
+        Random random = new Random();
+        int randomNumber = (random.nextInt(Board.MAX_SLOT - 1));
+        int randomNumberFinal = (randomNumber - (randomNumber * 2 * random.nextInt(1)));
+        setEffetto(randomNumberFinal);
+
+        while (Math.abs(randomNumberFinal) % 8 == 0) {
+            randomNumber = (random.nextInt(Board.MAX_SLOT - 1));
+            randomNumberFinal = (randomNumber - (randomNumber * 2 * random.nextInt(1)));
+            setEffetto(randomNumberFinal);
+        }
+
+        while (getPosizione() == 4 && randomNumberFinal % 4 == 0) {
+            randomNumber = (random.nextInt(Board.MAX_SLOT - 1));
+            randomNumberFinal = (randomNumber - (randomNumber * 2 * random.nextInt(1)));
+            setEffetto(randomNumberFinal);
+        }
+    }
 }
